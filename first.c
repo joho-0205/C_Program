8d1520c60a78bf32d0191a5514e0e8e6ae67fae2
joho-0205 <75572535+joho-0205@users.noreply.github.com>
2023-12-24 14:08:46 +0800

    Add files via upload

---
+++ first.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main(){ //int:一定要回傳整數 void:不回傳值
+    // data type
+    //int(%d), char(%c)(只能有一個字元,用單引號 ex.'e'), float(%f)(小數點只能有6位), bool, double(%f)(小數點能有12位)
+    int h,a;
+    a = 10;
+    h = 345;
+    printf("hello world %d",a);
+    return 0;
+}
